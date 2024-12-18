3b2e6e22abf7bbefa5f5d080c6100a660fc866bf
Frances Pasulohan <franpas12123@gmail.com>
2024-12-18 09:03:07 +0800

    updating directory page with directory component and category item component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+// import './App.css'
+import Directory from './components/directory/directory.component'
+
+const App = () => {
+
+  return (
+    <>
+      <Directory />
+    </>
+  )
+}
+
+export default App
