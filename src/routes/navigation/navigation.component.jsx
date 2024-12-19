daaf1d38289e29963ec6aaff6ca00a312bf6ece8
Frances Pasulohan <franpas12123@gmail.com>
2024-12-20 06:25:12 +0800

    create navigation component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { Link, Outlet } from 'react-router'
+import './navigation.styles.scss'
+import Logo from '@/assets/logo.svg?react'
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='navigation'>
+          <Link className='logo-container' to={'/'}>
+            <Logo className='logo' />
+          </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' key='shop' to='/shop'>
+            Shop
+          </Link>
+          <Link className='nav-link' key='sign-in' to='/sign-in'>
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default Navigation
