59d6be64256b7c42c73af817390bfcbad93397be
Frances Pasulohan <franpas12123@gmail.com>
2024-12-20 10:35:50 +0800

    move sign-up logic to SignUpForm component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '@/utils/firebase'
-
-const defaultFormFields = {
-  displayName: '',
-  email: '',
-  password: '',
-  confirmPassword: ''
-}
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '@/utils/firebase'
 
 const SignIn = () => {
   const handleSignInWithPopup = async () => {
@@ -15,33 +10,10 @@ const SignIn = () => {
     await createUserDocumentFromAuth(user)
   }
 
-  const [formFields, setFormFields] = useState(defaultFormFields)
-  const { displayName, email, password, confirmPassword } = formFields
-
-  const onChangeHandler = (e) => {
-    setFormFields({...formFields, [e.target.name]: e.target.value})
-  }
-  
   return (
     <>
       <div>I am Sign In page</div>
       <button onClick={handleSignInWithPopup}>Sign In with popup</button>
-
-      <form onSubmit={() => {}}>
-        <label>Display Name</label>
-        <input type='text' name='displayName' value={displayName} onChange={onChangeHandler} required  />
-
-        <label>Email</label>
-        <input type='email' name='email' value={email} onChange={onChangeHandler} required  />
-
-        <label>Password</label>
-        <input type='password' name='password' value={password} onChange={onChangeHandler} required  />
-
-        <label>Confirm Password</label>
-        <input type='password' name='confirmPassword' value={confirmPassword} onChange={onChangeHandler} required  />
-
-        <button type='submit'>Sign In</button>
-      </form>
     </>
   )
 }
