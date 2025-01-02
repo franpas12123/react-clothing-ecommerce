bd52fa9786e10e2ec80b4c32dfc0565f9d7f1c85
Frances Pasulohan <franpas12123@gmail.com>
2025-01-02 14:42:43 +0800

    create constants for firebase error codes

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const firebaseErrorCodes = {
+  'auth/email-already-in-use': 'Email already in use. Please use another email.',
+  'auth/weak-password': 'Password is too weak. Password should be at least 6 characters.',
+  'auth/invalid-email': 'Invalid email.',
+  'auth/user-not-found': 'User not found.',
+  'auth/wrong-passwords': 'Wrong password.',
+  'auth/too-many-requests': 'Too many requests, try again later.',
+  'auth/invalid-credential': 'Invalid credential.',
+}
